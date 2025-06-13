e7c475a294dbfe35f7d5a447de4cf04ad670a3fd
Aman Kumar <aman44008@gmail.com>
2025-06-13 11:30:52 +0530

    kjdfjg

--- controllers/userController.js
+++ controllers/userController.js
@@ -90,8 +90,10 @@ async function userLogin(req, res) {
       },
     });
 
-    
+
   } catch (error) {
+
+    
     console.error('Error during login:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
